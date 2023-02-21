d2dc97886dc51aacb1e24d1002d6996bc287c0b9
Mikael Ragna <mikael.previtera@gmail.com>
2023-02-21 09:56:02 -0300

    version 1.0

---
+++ descrição.md
@@ -0,0 +1,17 @@
+>Sobre nós
+
+Meu Pratim foi idealizado para ter o sabor de casa fora de casa. 
+Passe bons momentos com seus amigos, ou com aquela pessoa especial, no Meu Pratim.
+Buscamos transmitir a melhor experiência com comidas típas e caseiras que te lembram a infância quando frequentava festas típicas.
+Estamos localizados no Centro da cidade, para proporcionar o melhor da região para você. Mas se preferir, pode pedir no conforto da sua casa que chegamos até você.
+
+>Como montar o seu pratim!
+
+Selecione um dos tamanhos e adicione de acordo com o explicado abaixo:
+* Pratim P: pode adicionar uma mistura.
+* Pratim M: pode adicionar até duas misturas.
+* Pratim G: pode adicionar até três misturas.
+
+>Rodapé
+
+* Copyright Flor do Campo - 2023
--- pedido.js
+++ pedido.js
@@ -1,34 +1,3 @@
-// const mostrarEscolha = document.getElementById('finalizar__botao')
-
-// mostrarEscolha.onclick = function (){
-//   const tamanho = document.getElementById('tamanho').elements;
-//   const guarnicao = document.getElementById('guarnicao').elements;
-//   const mistura = document.getElementById('mistura').elements;
-//   const opcional = document.getElementById('opcional').elements;
-//   const bebida = document.getElementById('bebida').elements;
-
-//   let resultado = ``;
-//   for(let i = 0; i < 3; i++){
-//     if(tamanho[i].checked){
-//       resultado = resultado + tamanho[i].value + " "
-//     }
-//     if(guarnicao[i].checked){
-//       resultado = resultado + guarnicao[i].value + " "
-//     }
-//     if(mistura[i].checked){
-//       resultado = resultado + mistura[i].value + " "
-//     }
-//     if(opcional[i].checked){
-//       resultado = resultado + opcional[i].value + " "
-//     }
-//     if(bebida[i].checked){
-//       resultado = resultado + bebida[i].value + " "
-//     }
-//   }
-//   //document.getElementById("escolhas").innerHTML  = resultaddo
-//   console.log(resultado);
-// }
-
 let dados = {
   tamanhos: [
     { tipo: "Pratinho P", preco: 7 },
@@ -59,67 +28,90 @@ let opcional = document.querySelectorAll(".opcional");
 let bebida = document.querySelectorAll(".bebida");
 let btnPedidos = document.getElementById("finalizar__botao");
 
-//console.log(tamanho, guarnicao, mistura, opcional, bebida);
 
-function getCheckBoxArray(cssSelector) {
-  let result = document.querySelectorAll(cssSelector);
-  return result;
-}
+// function getCheckBoxArray(cssSelector) {
+//   let result = document.querySelectorAll(cssSelector);
+//   return result;
+// }
 
-tamanho.forEach((e, i) => {
+// const removeChecked = (arr) => {
+//   arr.forEach((element) => {
+//     if (element.getAttribute("checked")) {
+//       element.setAttribute("checked", false);
+//     }
+//     return;
+//   });
+// };
+
+
+
+tamanho.forEach((_, i) => {
   tamanho[i].addEventListener("change", (e) => {
     if (e.target.checked) {
-      removeChecked(getCheckBoxArray(`.${e.target.parentNode.classList[1]}`));
+      //removeChecked(getCheckBoxArray(`.${e.target.parentNode.classList[1]}`));
       pedido["tamanhos"] = dados["tamanhos"][e.target.value];
       console.log(pedido);
     }
   });
 });
 
-const removeChecked = (arr) => {
-  arr.forEach((element) => {
-    if (element.getAttribute("checked")) {
-      element.setAttribute("checked", false);
+guarnicao.forEach((_, i) => {
+  guarnicao[i].addEventListener("change", (e) => {
+    if (e.target.checked) {
+      //removeChecked(getCheckBoxArray(`.${e.target.parentNode.classList[1]}`));
+      pedido["guarnicoes"] = dados["guarnicoes"][e.target.value];
+      console.log(pedido);
     }
-    return;
   });
-};
+});
 
-// for (let checkbox of checkboxes) {
-//   checkbox.addEventListener("change", (e) => {
-//     if (e.target.checked) {
-//       removeChecked(getCheckBoxArray(`.${e.target.parentNode.classList[1]}`))
-//       pedido["tamanhos"] = dados["tamanhos"][e.target.value];
-//       console.log(getCheckBoxArray(`.${e.target.parentNode.classList[1]}`));
-//     }
-//     if(e.target.checked)
-//     {
-//       pedido["guarnicoes"] = dados["guarnicoes"][e.target.value];
-//       console.log(pedido);
-//     }
-//     if(e.target.checked)
-//     {
-//       pedido["misturas"] = dados["misturas"][e.target.value];
-//       console.log(pedido);
-//     }
-//     if(e.target.checked)
-//     {
-//       pedido["opcionais"] = dados["opcionais"][e.target.value];
-//       console.log(pedido);
-//     }
-//     if(e.target.checked)
-//     {
-//       pedido["bebidas"] = dados["bebidas"][e.target.value];
-//       console.log(pedido);
-//     }
-//   })
-// }
+mistura.forEach((_, i) => {
+  mistura[i].addEventListener("change", (e) => {
+    if (e.target.checked) {
+      //removeChecked(getCheckBoxArray(`.${e.target.parentNode.classList[1]}`));
+      pedido["misturas"] = dados["misturas"][e.target.value];
+      console.log(pedido);
+    }
+  });
+});
+
+opcional.forEach((_, i) => {
+  opcional[i].addEventListener("change", (e) => {
+    if (e.target.checked) {
+      //removeChecked(getCheckBoxArray(`.${e.target.parentNode.classList[1]}`));
+      pedido["opcionais"] = dados["opcionais"][e.target.value];
+      console.log(pedido);
+    }
+  });
+});
+
+bebida.forEach((_, i) => {
+  bebida[i].addEventListener("change", (e) => {
+    if (e.target.checked) {
+      //removeChecked(getCheckBoxArray(`.${e.target.parentNode.classList[1]}`));
+      pedido["bebidas"] = dados["bebidas"][e.target.value];
+      console.log(pedido);
+    }
+  });
+});
+
 btnPedidos.onclick = () => {
+  if (pedido.opcionais) {
+    op = `Opcional Selecionado: ${pedido.opcionais.tipo}`;
+  } else {
+    op = `Opcional Selecionado: Nenhum `;
+  }
+  if (pedido.bebidas) {
+    beb = `Bebida Selecionada: ${pedido.bebidas.tipo}`;
+  } else {
+    beb = `Bebida Selecionado: Nenhum `;
+  }
   let escreva = `
-  <p>Tamanho Selecionado: ${pedido.tamanhos.tipo}</p> 
-  <p>Guarnição Selecionado: ${pedido.guarnicoes.tipo}</p>
-  <p>Mistura Selecionada: ${pedido.misturas.tipo}</p>
-  <p>Opcional Selecionado: ${pedido.opcionais.tipo}</p>
-  <p>Bebida Selecionada: ${pedido.bebidas.tipo}</p>`;
-  document.getElementById("resultado").innerHTML = escreva;
-};
+  Tamanho Selecionado: ${pedido.tamanhos.tipo}
+  Guarnição Selecionado: ${pedido.guarnicoes.tipo}
+  Mistura Selecionada: ${pedido.misturas.tipo}
+  ${op}
+  ${beb}`;
+  return swal(escreva)
+}
+
